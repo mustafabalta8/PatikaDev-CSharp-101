4457890dee13ed43e2ce01eedf4e2e0d880c0cc4
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2022-03-10 17:45:41 +0300

    exception handling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// try
+// {
+//     Console.WriteLine("bi sayi giriniz.");
+//     int num = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Girdiginiz sayi: " + num);
+
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine("Hata: " + ex);
+
+
+// }
+// finally{
+//     Console.WriteLine("işlem tamam");
+// }
+
+
+try
+{
+    //int a = int.Parse(null);
+    //int a = int.Parse("test");
+    int a = int.Parse("-200000000000");
+    
+}
+catch (ArgumentNullException ex)
+{
+    Console.WriteLine("Boş değer girdiniz.");
+    Console.WriteLine(ex);
+}
+catch (FormatException ex)
+{
+    Console.WriteLine("Veri tipi uygun değil.");
+    Console.WriteLine(ex);
+}
+catch (OverflowException ex)
+{
+    Console.WriteLine("Çok küçük yada çok büyük bir değer girdiniz.");
+    Console.WriteLine(ex);
+}
+finally
+{
+    Console.WriteLine("İşlem başarıyla tamamlandı.");
+}
