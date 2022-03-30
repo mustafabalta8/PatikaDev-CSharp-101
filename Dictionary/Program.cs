4b70725247858736effd66bab1d3031305c23814
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2022-03-31 01:17:24 +0300

    dictionary example

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -28,3 +28,46 @@ foreach (var item in kullanicilar.Values)
     Console.WriteLine(item);
 }
 
+// example 
+
+int n = Convert.ToInt32(Console.ReadLine());
+Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+string[] names = new string[n];
+for (int i = 0; i < n; i++)
+{
+    string[] recordInput = Console.ReadLine().Split(' ');
+    phoneBook.Add(recordInput[0], recordInput[1]);
+}
+string nameForSearch = "";
+while ((nameForSearch = Console.ReadLine()) != null)
+{
+    if (phoneBook.ContainsKey(nameForSearch))
+    {
+        Console.WriteLine(nameForSearch + "=" + phoneBook[nameForSearch]);
+    }
+    else
+    {
+        Console.WriteLine("Not found");
+    }
+}
+/*
+for (int i = 0; i < n; i++)
+{
+    //read names to search in phoneBook
+    names[i] = Console.ReadLine();
+}
+foreach (var item in phoneBook)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if(item.Key == names[i]){
+            Console.WriteLine(item.Key+"="+item.Value);
+            break;
+        }        
+        if(i==n-1){
+            Console.WriteLine("Not found");
+        }
+        
+
+    }
+}*/
