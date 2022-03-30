e9bd56a5794291a6530966024d16aa300d0151ba
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2022-03-30 17:33:23 +0300

    generic list exercise

--- GenericList/Program.cs
+++ GenericList/Program.cs
@@ -87,21 +87,43 @@ namespace GenericList
 
             });
 
-              foreach (var item in kullanicilars)
-              {
-                  Console.WriteLine(item.Isim);
-                  Console.WriteLine(item.Soyad);
-                  Console.WriteLine(item.Yas);
-              }
-              foreach (var item in yeniListe)
-              {
-                  Console.WriteLine(item.Isim);
-                  Console.WriteLine(item.Soyad);
-                  Console.WriteLine(item.Yas);
-              }
-               
-                
+            foreach (var item in kullanicilars)
+            {
+                Console.WriteLine(item.Isim);
+                Console.WriteLine(item.Soyad);
+                Console.WriteLine(item.Yas);
+            }
+            foreach (var item in yeniListe)
+            {
+                Console.WriteLine(item.Isim);
+                Console.WriteLine(item.Soyad);
+                Console.WriteLine(item.Yas);
+            }
 
+        // hacerrank exercise
+        int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+
+        }
+        // hacerrank exercise 
+        public static void PrintArray<T>(T[] array)
+        {
+            foreach (var element in array)
+                Console.WriteLine(element);
         }
     }
 
@@ -115,4 +137,5 @@ namespace GenericList
         public string Soyad { get => soyad; set => soyad = value; }
         public int Yas { get => yas; set => yas = value; }
     }
+
 }
