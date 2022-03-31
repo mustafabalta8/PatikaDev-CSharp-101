be3ef603e1d96b94ee48946ca5fe263c1e145a9a
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2022-03-31 21:03:26 +0300

    Assignment02\Koleksiyonlar-Soru-3 is added

---
+++ Assignment02/Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan ve dizinin
+        elemanlarını sıralayan programı yazınız.*/
+
+        string vowels = "aeıioöuü";
+        List<char> vowelList = new List<char>();
+
+        Console.WriteLine("bir cümle giriniz!");
+        string inputSentece=Console.ReadLine().ToLower();
+
+        foreach(var item in inputSentece){
+            if(vowels.Contains(item)){
+                vowelList.Add(item);
+            }
+        }
+
+        if(vowelList.Count > 0){
+            foreach (var item in vowelList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        else{
+            Console.WriteLine("hiç sesli harf bulunamadı.");
+        }
+
+
+
+
+
+    }
+}
