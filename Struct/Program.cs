e3a3934f05655e614a2d1e691038ecc23ff65a30
Rochallor99 <33964145+Rochallor99@users.noreply.github.com>
2022-04-01 03:55:24 +0300

    Structs

---
+++ Struct/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen d = new Dikdortgen();
+            d.KisaKenar = 4;
+            d.UzunKenar = 15;
+            Console.WriteLine("Class Alan Hesaplama: {0}", d.AlanHesaplama());
+
+
+            Dikdortgen_s d_struct; //= new Dikdortgen_s(4, 15);
+            d_struct.KisaKenar=3;
+            d_struct.UzunKenar=4;
+            Console.WriteLine("Struct Alan Hesaplama: {0},", d_struct.AlanHesaplama());
+
+
+
+
+
+
+        }
+    }
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen()
+        {
+            KisaKenar = 4;
+            UzunKenar = 15;
+        }
+        public long AlanHesaplama()
+        {
+            return this.KisaKenar * this.UzunKenar;
+
+        }
+
+
+
+    }
+    struct Dikdortgen_s
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen_s(int kisaKenar, int uzunKenar)
+        {
+            KisaKenar = kisaKenar;
+            UzunKenar = uzunKenar;
+        }
+
+        public long AlanHesaplama()
+        {
+            return this.KisaKenar * this.UzunKenar;
+
+        }
+
+
+    }
+}
